5bb07e30d2d9cf45f706c93210e42a53fea5c976
SreeraG <sreerag.nair@built.io>
2018-05-25 19:58:13 +0530

    Did something....

--- src/expressServer.js
+++ src/expressServer.js
@@ -2,7 +2,7 @@
 
 const app = require('express')()
 const bodyparser = require('body-parser')
-const { createUser, addEntry, addNote } = require('./schema')
+const { addEntry, addNote, checkUser, checkNote, createUser } = require('./schema')
 
 app.use(bodyparser.json())
 
@@ -23,7 +23,29 @@ app.post('/insert-user', function (req, res) {
 
 app.post('/add-note', function (req, res) {
     var heavyobj = req.body
+
+    /*
+    FORMAT : 
     
+    {
+        userName : '',
+
+        note : {
+        title : '',
+        content : [
+            { 
+                isChecked : t/f,
+              entry : ''
+            }
+        ]
+    }
+    }
+
+    */
+    checkUser({ userName: heavyobj.userName }).then(function (doc, err) {
+        console.log(doc)
+    })
+
 })
 
 
@@ -36,3 +58,18 @@ app.post('/add-note', function (req, res) {
 app.listen(8000, function () {
     console.log("SERVER RUNNING IN 8000")
 })
+
+
+
+/*
+https://stackoverflow.com/questions/18867628/mongoose-deep-population-populate-a-populated-field
+https://github.com/Automattic/mongoose/issues/1377#issuecomment-15911192
+https://jaketrent.com/post/mongoose-population/
+http://frontendcollisionblog.com/mongodb/2016/01/24/mongoose-populate.html
+http://mongoosejs.com/docs/populate.html
+
+https://stackoverflow.com/questions/26008555/foreign-key-mongoose
+
+
+
+*/
